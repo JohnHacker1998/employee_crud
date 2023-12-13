4d83189e5a12314d878409ef17b6edc5e86e10ad
Yohannes Kindu Mekonnen <JohnHacker1998>
2023-12-13 17:39:29 +0300

    added exception handling

--- EmployeeManagement/Form1.cs
+++ EmployeeManagement/Form1.cs
@@ -20,8 +20,15 @@ namespace EmployeeManagement
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Computations computation=new Computations();           
-            dgvEmployee.DataSource = computation.GetAll();
+            try {
+                Computations computation = new Computations();
+                dgvEmployee.DataSource = computation.GetAll();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Internal error occured");
+            }
+           
  
         }
 
@@ -34,7 +41,7 @@ namespace EmployeeManagement
         {
 
         }
-
+        
         private void txtPhoneNumber_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
@@ -55,29 +62,37 @@ namespace EmployeeManagement
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            Computations computations = new Computations();
-            Employee employee = new Employee()
+            try
             {
-                Employee_ID = txtEmployeeID.Text,
-                Name = txtName.Text,
-                Phone = txtCountryCode.Text + txtPhoneNumber.Text,
-                Region = txtRegion.Text,
-                City = txtCity.Text,
-                Sub_City = txtSubCity.Text,
-                Location_Special_Name = txtLocationSpecialName.Text,
-                Data_Update_Date = DateTime.MinValue.Date.ToString(),
-                Active_Ind = cmbActiveInd.Text == "TRUE" ? true : false
-            };
-            if (computations.CheckEmployeeDuplication(employee.Employee_ID))
-            {
-                MessageBox.Show("Employee already exists!");
+                Computations computations = new Computations();
+                Employee employee = new Employee()
+                {
+                    Employee_ID = txtEmployeeID.Text,
+                    Name = txtName.Text,
+                    Phone = txtCountryCode.Text + txtPhoneNumber.Text,
+                    Region = txtRegion.Text,
+                    City = txtCity.Text,
+                    Sub_City = txtSubCity.Text,
+                    Location_Special_Name = txtLocationSpecialName.Text,
+                    Data_Update_Date = DateTime.MinValue.Date.ToString(),
+                    Active_Ind = cmbActiveInd.Text == "TRUE" ? true : false
+                };
+                if (computations.CheckEmployeeDuplication(employee.Employee_ID))
+                {
+                    MessageBox.Show("Employee already exists!");
+                }
+                else
+                {
+                    computations.Add(employee);
+                    dgvEmployee.DataSource = computations.GetAll();
+                    MessageBox.Show("Regestered Successful!");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                computations.Add(employee);
-                dgvEmployee.DataSource = computations.GetAll();
-                MessageBox.Show("Regestered Successful!");
+                MessageBox.Show("Internal error occured");
             }
+            
            
         }
 
@@ -88,43 +103,67 @@ namespace EmployeeManagement
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Computations computations = new Computations();
-            Employee employee = new Employee()
+            try
             {
-                Employee_ID = labelOriginalEmployoeeID.Text,
-                Name = txtName.Text,
-                Phone = txtCountryCode.Text + txtPhoneNumber.Text,
-                Region = txtRegion.Text,
-                City = txtCity.Text,
-                Sub_City = txtSubCity.Text,
-                Location_Special_Name = txtLocationSpecialName.Text,
-                Data_Update_Date = DateTime.Now.Date.ToString(),
-                Active_Ind = cmbActiveInd.Text == "TRUE" ? true : false
-            };
-            computations.Update(employee);
-            dgvEmployee.DataSource = computations.GetAll();
-            MessageBox.Show("Update is Successful!");
+                Computations computations = new Computations();
+                Employee employee = new Employee()
+                {
+                    Employee_ID = labelOriginalEmployoeeID.Text,
+                    Name = txtName.Text,
+                    Phone = txtCountryCode.Text + txtPhoneNumber.Text,
+                    Region = txtRegion.Text,
+                    City = txtCity.Text,
+                    Sub_City = txtSubCity.Text,
+                    Location_Special_Name = txtLocationSpecialName.Text,
+                    Data_Update_Date = DateTime.Now.Date.ToString(),
+                    Active_Ind = cmbActiveInd.Text == "TRUE" ? true : false
+                };
+                computations.Update(employee);
+                dgvEmployee.DataSource = computations.GetAll();
+                MessageBox.Show("Update is Successful!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Internal error occured");
+            }
+         
 
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            Computations computations = new Computations();
-            dgvEmployee.DataSource = computations.GetByName(txtSearchByName.Text);
+            try
+            {
+                Computations computations = new Computations();
+                dgvEmployee.DataSource = computations.GetByName(txtSearchByName.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Internal error occured");
+            }
+           
 
         }
 
         private void dgvEmployee_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            txtEmployeeID.Text = dgvEmployee.CurrentRow.Cells[0].Value.ToString();
-            labelOriginalEmployoeeID.Text= dgvEmployee.CurrentRow.Cells[0].Value.ToString();
-            txtName.Text = dgvEmployee.CurrentRow.Cells[1].Value.ToString();
-            txtPhoneNumber.Text = dgvEmployee.CurrentRow.Cells[2].Value.ToString().Substring(4);
-            txtRegion.Text = dgvEmployee.CurrentRow.Cells[3].Value.ToString();
-            txtCity.Text = dgvEmployee.CurrentRow.Cells[4].Value.ToString();
-            txtSubCity.Text = dgvEmployee.CurrentRow.Cells[5].Value.ToString();
-            txtLocationSpecialName.Text = dgvEmployee.CurrentRow.Cells[6].Value.ToString();
-            cmbActiveInd.Text = dgvEmployee.CurrentRow.Cells[8].Value.ToString() == "True" ? "TRUE" : "FALSE";
+            try
+            {
+                txtEmployeeID.Text = dgvEmployee.CurrentRow.Cells[0].Value.ToString();
+                labelOriginalEmployoeeID.Text = dgvEmployee.CurrentRow.Cells[0].Value.ToString();
+                txtName.Text = dgvEmployee.CurrentRow.Cells[1].Value.ToString();
+                txtPhoneNumber.Text = dgvEmployee.CurrentRow.Cells[2].Value.ToString().Substring(4);
+                txtRegion.Text = dgvEmployee.CurrentRow.Cells[3].Value.ToString();
+                txtCity.Text = dgvEmployee.CurrentRow.Cells[4].Value.ToString();
+                txtSubCity.Text = dgvEmployee.CurrentRow.Cells[5].Value.ToString();
+                txtLocationSpecialName.Text = dgvEmployee.CurrentRow.Cells[6].Value.ToString();
+                cmbActiveInd.Text = dgvEmployee.CurrentRow.Cells[8].Value.ToString() == "True" ? "TRUE" : "FALSE";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Internal error occured");
+            }
+           
 
 
         }
